d6ba109d840d2cb3357f712313ab85a2cb6ff6a3
brk <brk@iastate.edu>
2017-08-11 09:16:03 -0500

    chapter 1 JavaScript end

--- HeadFirst_JavaScript/chapter1/code.js
+++ HeadFirst_JavaScript/chapter1/code.js
@@ -1,11 +1,14 @@
-scoops = 5;
-while (scoops > 0) {
-	document.write("Another scoop!<br>");
-	if (scoops < 3) {
-		alert("Ice cream is running low!");
-	} else if (scoops >= 5) {
-		alert("Eat faster, the ice cream is going to melt!");
+var word = "bottles";
+var count = 99;
+	while (count > 0) {
+		console.log(count + " " + word + " of beer on the wall");
+		console.log(count + " " + word + " of beer,");
+		console.log("Take one down, pass it around,");
+		count = count - 1;
+		if (count > 0) {
+			console.log(count + " " + word + " of beer on the wall.");
+		}
+		else {
+			console.log("No more " + word + " of beer on the wall.");
+		}
 	}
-	scoops = scoops - 1;
-}
-document.write("Life without ice cream isn't the same");
